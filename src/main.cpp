f524387881ad18586aec6947b9c79e45a863c533
Laurine-sl <laurine.suel@edu.esiee.fr>
2023-11-22 13:47:57 +0100

    ajout de fractal

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "random.hpp"
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <complex>
 
 void green(sil::Image &img)
 {
@@ -342,6 +343,68 @@ void glitch(sil::Image &img)
     }
 }
 
+void fractal_pixelized(sil::Image &img)
+{
+    for (float x{0}; x < img.width(); x++)
+    {
+        for (float y{0}; y < img.height(); y++)
+        {
+            std::complex<float> c{(x / 250 - 1) * 2, (y / 250 - 1) * 2};
+            std::complex<float> z{0.f, 0.f};
+            float color = 1;
+            for (int i{0}; i < 100; i++)
+            {
+                z = z * z + c;
+                color -= 0.0001;
+                if (std::abs(z) > 2)
+                {
+                    img.pixel(x, y).r = 0.f;
+                    img.pixel(x, y).b = 0.f;
+                    img.pixel(x, y).g = 0.f;
+                    break;
+                }
+            }
+            if (std::abs(z) <= 2)
+            {
+                img.pixel(x, y).r = color;
+                img.pixel(x, y).b = color;
+                img.pixel(x, y).g = color;
+            }
+        }
+    }
+}
+
+void fractal(sil::Image &img)
+{
+    for (float x{0}; x < img.width(); x++)
+    {
+        for (float y{0}; y < img.height(); y++)
+        {
+            std::complex<float> c{(x / 250 - 1) * 2, (y / 250 - 1) * 2};
+            std::complex<float> z{0.f, 0.f};
+            float color = 0;
+            for (int i{0}; i < 100; i++)
+            {
+                z = z * z + c;
+                color += 0.04;
+                if (std::abs(z) > 2)
+                {
+                    img.pixel(x, y).r = color;
+                    img.pixel(x, y).b = color;
+                    img.pixel(x, y).g = color;
+                    break;
+                }
+            }
+            if (std::abs(z) <= 2)
+            {
+                img.pixel(x, y).r = 0.8;
+                img.pixel(x, y).b = 0.7;
+                img.pixel(x, y).g = 0.1;
+            }
+        }
+    }
+}
+
 int main()
 {
     {
@@ -445,5 +508,15 @@ int main()
         sil::Image image("img/logo.png");
         glitch(image);
         image.save("output/glitch.png");
+    }
+    {
+        sil::Image image(500, 500);
+        fractal_pixelized(image);
+        image.save("output/fractal_pixelized.png");
+    }
+    {
+        sil::Image image(500, 500);
+        fractal(image);
+        image.save("output/fractal.png");
     }
 }
