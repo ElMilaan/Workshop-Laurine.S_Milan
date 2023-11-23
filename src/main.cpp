d46d108d1bf468e600a0c7bf7dfeb2ea507a5149
Laurine-sl <laurine.suel@edu.esiee.fr>
2023-11-23 12:12:28 +0100

    tri de pixels

--- src/main.cpp
+++ src/main.cpp
@@ -546,6 +546,16 @@ void tramage(sil::Image &img, float seuil)
         }
     }
 }
+void sort(sil::Image &img)
+{
+    for (int i = 0; i < 300; i++)
+    {
+        int tab_begin = (i / 300.f) * img.pixels().size();
+        int tab_end = ((i + 1) / 300.f) * img.pixels().size();
+        std::sort(img.pixels().begin() + tab_begin, img.pixels().begin() + tab_end, [](glm::vec3 const &color1, glm::vec3 const &color2)
+                  { return color1.r + color1.g + color1.b < color2.r + color2.g + color2.b; });
+    }
+}
 
 int main()
 {
@@ -676,5 +686,10 @@ int main()
         sil::Image image("img/photo.jpg");
         tramage(image, 2.2);
         image.save("output/tramage.jpg");
+    }
+    {
+        sil::Image image{"img/logo.png"};
+        sort(image);
+        image.save("output/sort.png");
     }
 }
